446a15bedf4ea60f58615f74dd964dc5e083a68a
ontai-ag <ontai.market@gmail.com>
2025-05-10 17:18:28 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { componentTagger } from 'lovable-tagger';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: mode === 'production' ? '/vite_react_shadcn_ts/' : '/',
   server: {
     host: "::",
     port: 8080,
