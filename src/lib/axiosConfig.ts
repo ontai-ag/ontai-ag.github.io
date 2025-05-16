ca6a3c015d065246a885eab2846e56c71bd185dc
ontai-ag <ontai.market@gmail.com>
2025-05-17 00:16:06 +0500

    r1

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -4,5 +4,5 @@ axios.defaults.baseURL = import.meta.env.MODE === 'development'
   ? '/api'
   : 'https://api.ontai.kz';
 
-axios.defaults.headers.common['Referrer-Policy'] = 'strict-origin-when-cross-origin';
+axios.defaults.headers.common['referrer-policy'] = 'strict-origin-when-cross-origin';
 axios.defaults.withCredentials = true;
